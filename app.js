fd0d1d2ce47ddfd029e60e635175b9968163ce55
Ralpmike <ralphmike996@gmail.com>
2025-06-26 14:34:10 +0100

    responding to url params

--- app.js
+++ app.js
@@ -25,6 +25,25 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+
+  if (tours.length < id) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  const tour = tours.find((el) => el.id === id);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
