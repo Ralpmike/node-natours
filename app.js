c15f12ccdfd3ecca8d8a01c8660a1ee1b4b29912
Ralpmike <ralphmike996@gmail.com>
2025-06-28 15:03:34 +0100

    environment variables

--- app.js
+++ app.js
@@ -7,9 +7,12 @@ const userRouter = require('./routes/userRouter.routes');
 const app = express();
 
 const staticFilePath = path.join(__dirname, 'public');
+
 // ?Middlewares
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
-app.use(morgan('dev'));
 app.use(express.static(staticFilePath));
 app.use((req, res, next) => {
   console.log('Hello from the middleware 👋');
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const app = require('./app');
 const dotenv = require('dotenv');
-
 dotenv.config();
+const app = require('./app');
+
+// console.log(process.env);
 
 // ?Starting the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log('App running on port 3000...');
+  console.log(`App running on port ${port}...`);
 });
