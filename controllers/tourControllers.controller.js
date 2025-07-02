25eed07abd0aefcc912b3f505adfc75fda5357de
Ralpmike <ralphmike996@gmail.com>
2025-07-02 09:57:03 +0100

    reading documents

--- controllers/tourControllers.controller.js
+++ controllers/tourControllers.controller.js
@@ -1,27 +1,42 @@
 const Tour = require('../models/tourModel');
 
 //?get all tours
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestedAt,
-    // results: tours.length,
-    // data: {
-    //   tours,
-    // },
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find({});
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error,
+    });
+  }
 };
 
 //?get specific tour
-exports.getTour = (req, res) => {
-  const id = parseInt(req.params.id, 10);
-
-  res.status(200).json({
-    status: 'success',
-    // data: {
-    //   tour,
-    // },
-  });
+exports.getTour = async (req, res) => {
+  try {
+    const { id } = req.params;
+    //? findByid is a short hnad for findOne: findOne({ _id: id })
+    const tour = await Tour.findById(id);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error,
+    });
+  }
 };
 
 //?create tour
@@ -47,16 +62,26 @@ exports.createTour = async (req, res) => {
 };
 
 //?update tour
-exports.updateTour = (req, res) => {
-  const id = parseInt(req.params.id, 10);
+exports.updateTour = async (req, res) => {
+  try {
+    const updatedTour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
 
-  res.status(200).json({
-    status: 'success',
-    message: 'Updated successfully',
-    // data: {
-    //   tour: updateTour,
-    // },
-  });
+    res.status(200).json({
+      status: 'success',
+      message: 'Updated successfully',
+      data: {
+        tour: updatedTour,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error,
+    });
+  }
 };
 
 //?delete tour
