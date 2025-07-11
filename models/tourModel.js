5b65a4028277c75e3a8f8414f949d4fe07a3faa0
Ralpmike <ralphmike996@gmail.com>
2025-07-11 16:25:35 +0100

    mongoose document middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const slugify = require('slugify');
 
 // JSON.stringify(doc.toObject({ virtuals: true }));
 
@@ -10,6 +12,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -77,6 +80,24 @@ const tourSchema = new mongoose.Schema(
 
 // tourSchema.virtual('durationWeeks').get(function () {
 //   return this.duration / 7;
+// });
+
+//?DOCUMENTMIDDLEWARE: runs before .save() and .create()
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+
+  next();
+});
+
+// tourSchema.pre('save', (next) => {
+//   console.log('Will save document...');
+
+//   next();
+// });
+
+// tourSchema.post('save', (doc, next) => {
+//   console.log(doc);
+//   next();
 // });
 
 const Tour = mongoose.model('Tour', tourSchema);
