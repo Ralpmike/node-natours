e1238164e6b9a274d1550a3a7e40b163abb71c54
Ralpmike <ralphmike996@gmail.com>
2025-07-19 13:07:28 +0100

    better errors and refactoring

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const path = require('path');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const qs = require('qs');
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController.controller');
 const tourRouter = require('./routes/tourRouter.routes');
-
 const userRouter = require('./routes/userRouter.routes');
 
 const app = express();
@@ -26,25 +27,13 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.use((req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `Can't find ${req.originalUrl} on this server!`,
-  // });
-  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-  err.statusCode = 404;
-  err.status = 'fail';
+  // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  // err.statusCode = 404;
+  // err.status = 'fail';
 
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
