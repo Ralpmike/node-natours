58d8a9fd5c56f260d001f8a53018e3eb29a6a26a
Ralpmike <ralphmike996@gmail.com>
2025-07-20 19:38:57 +0100

    handling invalid database id

--- controllers/errorController.controller.js
+++ controllers/errorController.controller.js
@@ -1,3 +1,10 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -15,10 +22,17 @@ const sendErrorProd = (err, res) => {
       message: err.message,
     });
   }
-
   // Programming or other unknown error: don't leak error details
+
+  //?1) Log error
   console.error('ERROR 💥', err);
-  return res.status(500).json({
+
+  //?2) Send generic message
+  // Note: In production, we don't want to send the error details to the client
+  // as it may contain sensitive information.
+  // Instead, we send a generic message.
+  // This is a security measure to prevent information leakage.
+  res.status(500).json({
     status: 'error',
     message: 'Something went very wrong!',
   });
@@ -29,9 +43,22 @@ module.exports = (err, req, res, next) => {
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
+    console.log('error', err);
+
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
+    //? let error = { ...err } this fails because it could copy the prototype chain
+    let error = {
+      ...err,
+      message: err.message,
+      name: err.name,
+      stack: err.stack,
+    }; // This creates a shallow copy of the error object
+    console.log('error1', error);
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
     // For production, we can send a more generic error message
-    sendErrorProd(err, res);
+    console.log('error2', error);
+
+    sendErrorProd(error, res);
   }
 };
