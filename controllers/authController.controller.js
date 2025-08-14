86995516474d5756e1afe00066156848df9a11bb
Ralpmike <ralphmike996@gmail.com>
2025-08-14 16:39:04 +0100

    password reset functionality

--- controllers/authController.controller.js
+++ controllers/authController.controller.js
@@ -1,4 +1,5 @@
 const { promisify } = require('util');
+const crypto = require('crypto');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
@@ -153,10 +154,38 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   }
 });
 
-exports.resetPassword = (req, res, next) => {
+exports.resetPassword = catchAsync(async (req, res, next) => {
   //? 1) Get user based on the token
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex');
+
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() },
+  });
   //? 2) If token has not expired, and there is user, set the new password
+  if (!user) {
+    return next(new AppError('Token is invalid or has expired', 400));
+  }
   //? 3) Update changedPasswordAt property for the user
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+  await user.save();
+
   //? 4) Log the user in, send JWT
+  const token = signInToken({ id: user._id, name: user.name });
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
+
   //? 5) Send JWT only to client
-};
+  res.json({
+    status: 'success',
+    token,
+  });
+});
--- models/userModel.js
+++ models/userModel.js
@@ -65,6 +65,13 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
+userSchema.pre('save', function (next) {
+  if (!this.isModified('password') || this.isNew) return next();
+
+  this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
 userSchema.methods.confirmPassword = async function (
   candidatePassword,
   userPassword,
